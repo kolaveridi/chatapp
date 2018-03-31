055ca1a4ae60d14b9766acd54f49f9133186e2c4
kolaveridi <coderboysatya@gmail.com>
2018-03-31 23:01:43 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -15,15 +15,33 @@ var server=http.createServer(app);
 var io=socketIO(server);
 io.on('connection',(socket)=>{
   console.log('New user conected');
-  
-  socket.on('createMessage',(message)=>{
-    console.log('createMessage',message);
-    io.emit('newMessage',{
-       from:message.from,
-       text:message.text,
-       createdAt:new Date().getTime()
-    });
-  });
+      socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to Chat App',
+        createdAt:new Date().getTime()
+      });
+      socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user joined',
+        createdAt:new Date().getTime()
+      });
+
+      socket.on('createMessage',(message)=>{
+        console.log('createMessage',message);
+        io.emit('newMessage',{
+         from:message.from,
+         text:message.text,
+         createdAt:new Date().getTime()
+         });
+         });
+
+    //this will emit to all except the one which emits
+
+    //socket.broadcast.emit('newMessage',{
+      //from:message.from,
+      //text:message.text,
+      //createdAt:new Date().getTime()
+    //});
   socket.on('disconnect',()=>{
     console.log('User was Disconnected');
   });
